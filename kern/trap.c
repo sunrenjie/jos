8f25cbd230e1dfd0983074afe1bc6611fa82ac78
Renjie Sun <renjiesun.MacBook-Air@gmail.com>
2015-02-15 20:21:36 +0800

    [lab3] Ex.6: to invoke kernel monitor on T_BRKPT

--- kern/trap.c
+++ kern/trap.c
@@ -151,6 +151,8 @@ trap_dispatch(struct Trapframe *tf)
 {
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
+	if (tf->tf_trapno == T_BRKPT)
+		monitor(tf);
 	
 
 	// Unexpected trap: The user process or the kernel has a bug.
