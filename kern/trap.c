220a34b570d8f2d1803c41151758b0e64b5ffd93
Renjie Sun <renjiesun.MacBook-Air@gmail.com>
2015-02-16 13:56:13 +0800

    [lab3] Ex.8 Added support of env for user programs
    In addition, trap handler dispatching code is improved. In particular,
    added the missing 'return;' clause to handled cases; we should have done
    that in the previous commit.

--- kern/trap.c
+++ kern/trap.c
@@ -151,14 +151,17 @@ trap_dispatch(struct Trapframe *tf)
 {
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
-	if (tf->tf_trapno == T_BRKPT)
+	switch(tf->tf_trapno) {
+	case T_BRKPT:
 		monitor(tf);
-	else if (tf->tf_trapno == T_SYSCALL)
+		return;
+	case T_SYSCALL:
 		tf->tf_regs.reg_eax = syscall(
 			tf->tf_regs.reg_eax, tf->tf_regs.reg_edx,
 			tf->tf_regs.reg_ecx, tf->tf_regs.reg_ebx,
 			tf->tf_regs.reg_edi, tf->tf_regs.reg_esi);
-	
+		return;
+	}
 
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
