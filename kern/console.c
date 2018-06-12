b3f716b288b4642b11fd3ede1d03836046c1d1a3
Renjie Sun <sunrenjie6@gmail.com>
2018-06-12 18:05:53 +0800

    Improvements to kernel console I/O
    This patch is literally the difference between the version 2017 and 2006
    course ware.
    
    Main changes are:
    1. cons_putc(): write console output to serial, as part of hooking
    up VM serial to QEMU stdio (see its command line in GNUmakefile), for
    the better experiences.
    2. kbd_proc_data(): disable mouse data.
    
    Additional changes:
    1. More C99-compliant designated initialisers. See also:
    https://stackoverflow.com/questions/9849719/what-do-square-brackets-mean-in-array-initialization-in-c
    2. Trivial function definition/declaration moving-arounds.

--- kern/console.c
+++ kern/console.c
@@ -9,15 +9,25 @@
 #include <kern/console.h>
 #include <kern/picirq.h>
 
+static void cons_intr(int (*proc)(void));
+static void cons_putc(int c);
 
-void cons_intr(int (*proc)(void));
-
+// Stupid I/O delay routine necessitated by historical PC design flaws
+static void
+delay(void)
+{
+	inb(0x84);
+	inb(0x84);
+	inb(0x84);
+	inb(0x84);
+}
 
 /***** Serial I/O code *****/
 
 #define COM1		0x3F8
 
 #define COM_RX		0	// In:	Receive buffer (DLAB=0)
+#define COM_TX		0	// Out: Transmit buffer (DLAB=0)
 #define COM_DLL		0	// Out: Divisor Latch Low (DLAB=1)
 #define COM_DLM		1	// Out: Divisor Latch High (DLAB=1)
 #define COM_IER		1	// Out: Interrupt Enable Register
@@ -33,10 +43,12 @@ void cons_intr(int (*proc)(void));
 #define	  COM_MCR_OUT2	0x08	// Out2 complement
 #define COM_LSR		5	// In:	Line Status Register
 #define   COM_LSR_DATA	0x01	//   Data available
+#define   COM_LSR_TXRDY	0x20	//   Transmit buffer avail
+#define   COM_LSR_TSRE	0x40	//   Transmitter off
 
 static bool serial_exists;
 
-int
+static int
 serial_proc_data(void)
 {
 	if (!(inb(COM1+COM_LSR) & COM_LSR_DATA))
@@ -51,12 +63,25 @@ serial_intr(void)
 		cons_intr(serial_proc_data);
 }
 
-void
+static void
+serial_putc(int c)
+{
+	int i;
+
+	for (i = 0;
+	     !(inb(COM1 + COM_LSR) & COM_LSR_TXRDY) && i < 12800;
+	     i++)
+		delay();
+
+	outb(COM1 + COM_TX, c);
+}
+
+static void
 serial_init(void)
 {
 	// Turn off the FIFO
 	outb(COM1+COM_FCR, 0);
-	
+
 	// Set speed; requires DLAB latch
 	outb(COM1+COM_LCR, COM_LCR_DLAB);
 	outb(COM1+COM_DLL, (uint8_t) (115200 / 9600));
@@ -87,16 +112,6 @@ serial_init(void)
 // For information on PC parallel port programming, see the class References
 // page.
 
-// Stupid I/O delay routine necessitated by historical PC design flaws
-static void
-delay(void)
-{
-	inb(0x84);
-	inb(0x84);
-	inb(0x84);
-	inb(0x84);
-}
-
 static void
 lpt_putc(int c)
 {
@@ -118,7 +133,7 @@ static unsigned addr_6845;
 static uint16_t *crt_buf;
 static uint16_t crt_pos;
 
-void
+static void
 cga_init(void)
 {
 	volatile uint16_t *cp;
@@ -135,7 +150,7 @@ cga_init(void)
 		*cp = was;
 		addr_6845 = CGA_BASE;
 	}
-	
+
 	/* Extract cursor location */
 	outb(addr_6845, 14);
 	pos = inb(addr_6845 + 1) << 8;
@@ -148,7 +163,7 @@ cga_init(void)
 
 
 
-void
+static void
 cga_putc(int c)
 {
 	// if no attribute given, then use black on white
@@ -184,7 +199,7 @@ cga_putc(int c)
 	if (crt_pos >= CRT_SIZE) {
 		int i;
 
-		memcpy(crt_buf, crt_buf + CRT_COLS, (CRT_SIZE - CRT_COLS) * sizeof(uint16_t));
+		memmove(crt_buf, crt_buf + CRT_COLS, (CRT_SIZE - CRT_COLS) * sizeof(uint16_t));
 		for (i = CRT_SIZE - CRT_COLS; i < CRT_SIZE; i++)
 			crt_buf[i] = 0x0700 | ' ';
 		crt_pos -= CRT_COLS;
@@ -212,21 +227,21 @@ cga_putc(int c)
 
 #define E0ESC		(1<<6)
 
-static uint8_t shiftcode[256] = 
+static uint8_t shiftcode[256] =
 {
-	[0x1D] CTL,
-	[0x2A] SHIFT,
-	[0x36] SHIFT,
-	[0x38] ALT,
-	[0x9D] CTL,
-	[0xB8] ALT
+	[0x1D] = CTL,
+	[0x2A] = SHIFT,
+	[0x36] = SHIFT,
+	[0x38] = ALT,
+	[0x9D] = CTL,
+	[0xB8] = ALT
 };
 
-static uint8_t togglecode[256] = 
+static uint8_t togglecode[256] =
 {
-	[0x3A] CAPSLOCK,
-	[0x45] NUMLOCK,
-	[0x46] SCROLLLOCK
+	[0x3A] = CAPSLOCK,
+	[0x45] = NUMLOCK,
+	[0x46] = SCROLLLOCK
 };
 
 static uint8_t normalmap[256] =
@@ -242,15 +257,15 @@ static uint8_t normalmap[256] =
 	NO,   NO,   NO,   NO,   NO,   NO,   NO,   '7',	// 0x40
 	'8',  '9',  '-',  '4',  '5',  '6',  '+',  '1',
 	'2',  '3',  '0',  '.',  NO,   NO,   NO,   NO,	// 0x50
-	[0x97] KEY_HOME,	[0x9C] '\n' /*KP_Enter*/,
-	[0xB5] '/' /*KP_Div*/,	[0xC8] KEY_UP,
-	[0xC9] KEY_PGUP,	[0xCB] KEY_LF,
-	[0xCD] KEY_RT,		[0xCF] KEY_END,
-	[0xD0] KEY_DN,		[0xD1] KEY_PGDN,
-	[0xD2] KEY_INS,		[0xD3] KEY_DEL
+	[0xC7] = KEY_HOME,	      [0x9C] = '\n' /*KP_Enter*/,
+	[0xB5] = '/' /*KP_Div*/,      [0xC8] = KEY_UP,
+	[0xC9] = KEY_PGUP,	      [0xCB] = KEY_LF,
+	[0xCD] = KEY_RT,	      [0xCF] = KEY_END,
+	[0xD0] = KEY_DN,	      [0xD1] = KEY_PGDN,
+	[0xD2] = KEY_INS,	      [0xD3] = KEY_DEL
 };
 
-static uint8_t shiftmap[256] = 
+static uint8_t shiftmap[256] =
 {
 	NO,   033,  '!',  '@',  '#',  '$',  '%',  '^',	// 0x00
 	'&',  '*',  '(',  ')',  '_',  '+',  '\b', '\t',
@@ -263,31 +278,31 @@ static uint8_t shiftmap[256] =
 	NO,   NO,   NO,   NO,   NO,   NO,   NO,   '7',	// 0x40
 	'8',  '9',  '-',  '4',  '5',  '6',  '+',  '1',
 	'2',  '3',  '0',  '.',  NO,   NO,   NO,   NO,	// 0x50
-	[0x97] KEY_HOME,	[0x9C] '\n' /*KP_Enter*/,
-	[0xB5] '/' /*KP_Div*/,	[0xC8] KEY_UP,
-	[0xC9] KEY_PGUP,	[0xCB] KEY_LF,
-	[0xCD] KEY_RT,		[0xCF] KEY_END,
-	[0xD0] KEY_DN,		[0xD1] KEY_PGDN,
-	[0xD2] KEY_INS,		[0xD3] KEY_DEL
+	[0xC7] = KEY_HOME,	      [0x9C] = '\n' /*KP_Enter*/,
+	[0xB5] = '/' /*KP_Div*/,      [0xC8] = KEY_UP,
+	[0xC9] = KEY_PGUP,	      [0xCB] = KEY_LF,
+	[0xCD] = KEY_RT,	      [0xCF] = KEY_END,
+	[0xD0] = KEY_DN,	      [0xD1] = KEY_PGDN,
+	[0xD2] = KEY_INS,	      [0xD3] = KEY_DEL
 };
 
 #define C(x) (x - '@')
 
-static uint8_t ctlmap[256] = 
+static uint8_t ctlmap[256] =
 {
-	NO,      NO,      NO,      NO,      NO,      NO,      NO,      NO, 
-	NO,      NO,      NO,      NO,      NO,      NO,      NO,      NO, 
+	NO,      NO,      NO,      NO,      NO,      NO,      NO,      NO,
+	NO,      NO,      NO,      NO,      NO,      NO,      NO,      NO,
 	C('Q'),  C('W'),  C('E'),  C('R'),  C('T'),  C('Y'),  C('U'),  C('I'),
 	C('O'),  C('P'),  NO,      NO,      '\r',    NO,      C('A'),  C('S'),
-	C('D'),  C('F'),  C('G'),  C('H'),  C('J'),  C('K'),  C('L'),  NO, 
+	C('D'),  C('F'),  C('G'),  C('H'),  C('J'),  C('K'),  C('L'),  NO,
 	NO,      NO,      NO,      C('\\'), C('Z'),  C('X'),  C('C'),  C('V'),
 	C('B'),  C('N'),  C('M'),  NO,      NO,      C('/'),  NO,      NO,
-	[0x97] KEY_HOME,
-	[0xB5] C('/'),		[0xC8] KEY_UP,
-	[0xC9] KEY_PGUP,	[0xCB] KEY_LF,
-	[0xCD] KEY_RT,		[0xCF] KEY_END,
-	[0xD0] KEY_DN,		[0xD1] KEY_PGDN,
-	[0xD2] KEY_INS,		[0xD3] KEY_DEL
+	[0x97] = KEY_HOME,
+	[0xB5] = C('/'),		[0xC8] = KEY_UP,
+	[0xC9] = KEY_PGUP,		[0xCB] = KEY_LF,
+	[0xCD] = KEY_RT,		[0xCF] = KEY_END,
+	[0xD0] = KEY_DN,		[0xD1] = KEY_PGDN,
+	[0xD2] = KEY_INS,		[0xD3] = KEY_DEL
 };
 
 static uint8_t *charcode[4] = {
@@ -305,10 +320,14 @@ static int
 kbd_proc_data(void)
 {
 	int c;
-	uint8_t data;
+	uint8_t stat, data;
 	static uint32_t shift;
 
-	if ((inb(KBSTATP) & KBS_DIB) == 0)
+	stat = inb(KBSTATP);
+	if ((stat & KBS_DIB) == 0)
+		return -1;
+	// Ignore data from mouse.
+	if (stat & KBS_TERR)
 		return -1;
 
 	data = inb(KBDATAP);
@@ -355,7 +374,7 @@ kbd_intr(void)
 	cons_intr(kbd_proc_data);
 }
 
-void
+static void
 kbd_init(void)
 {
 	// Drain the kbd buffer so that Bochs generates interrupts.
@@ -380,7 +399,7 @@ static struct {
 
 // called by device interrupt routines to feed input characters
 // into the circular console input buffer.
-void
+static void
 cons_intr(int (*proc)(void))
 {
 	int c;
@@ -417,9 +436,10 @@ cons_getc(void)
 }
 
 // output a character to the console
-void
+static void
 cons_putc(int c)
 {
+	serial_putc(c);
 	lpt_putc(c);
 	cga_putc(c);
 }
